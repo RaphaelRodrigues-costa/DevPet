802f17fea71f1ca73c78be0a3f70b14c705a9ec7
raphael-costa <raphael_rpc@outlook.com>
2022-11-21 09:50:31 -0300

    ajuste na controller de page

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,8 +1,7 @@
 import { Request, Response } from "express";
 
 export const home = (req: Request, res: Response) => {
-    res.send('Home no controller');
-    // return res.render('pages/page');
+    return res.render('pages/page');
 }
 
 export const dogs = (req: Request, res: Response) => {

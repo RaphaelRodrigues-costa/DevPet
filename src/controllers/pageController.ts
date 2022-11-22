d3875559dea37b9e40761a5917039c13c6f7ee1a
raphael-costa <raphael_rpc@outlook.com>
2022-11-22 09:33:12 -0300

    ajustes na controller, para render

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,20 +1,50 @@
 import { Request, Response } from "express";
 
+/**
+ * Aqui a função é utilizada no rander para definir onde o usuario esta em nosso menu de navegação,
+ * depois é setado no nosso html em Header <nav>
+ */
+import {createMenuObject} from "../helpers/createMenuObject"
+
+
 export const home = (req: Request, res: Response) => {
-    return res.render('pages/page');
+    return res.render('pages/page', {
+        menu: createMenuObject('all'),
+        banner: {
+            title: "Todos os animais",
+            background: 'allanimals.jpg'
+        }
+    });
 }
 
 export const dogs = (req: Request, res: Response) => {
-    res.send('Dogs no controller');
-    // return res.render('pages/page');
+    return res.render('pages/page', {
+        menu: createMenuObject('dog'),
+        banner: {
+            title: "Cachorro",
+            background: 'banner_dog.jpg'
+        }
+    });
 }
 
 
 export const cats = (req: Request, res: Response) => {
-    // return res.render('pages/page');
+    return res.render('pages/page', {
+        menu: createMenuObject('cat'),
+        banner: {
+            title: "Gatos",
+            background: 'banner_cat.jpg'
+        }
+    });
 }
 
 
 export const fishes = (req: Request, res: Response) => {
-    // return res.render('pages/page');
+    return res.render('pages/page', {
+        menu: createMenuObject('fish'),
+        banner: {
+            title: "Peixes",
+            background: 'banner_fish.jpg'
+        }
+    });
 }
